454265b615cd38dd3bcda1af27bab625723b1dd9
Yeeanj <sesju0527@gamil.com>
2023-11-01 13:25:05 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function Header(props) {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>logo</h1>
+      {/* <Nav /> */}
+    </header>
+  );
+}
+
+function Nav(props) {
+  // map()함수를 사용한 배열 출력
+  const list = props.nav.map((item, index) => (
+    <li key={index}>
+      <a href={"/sub/" + item.title + "html"}>{item.title}</a>
+    </li>
+  ));
+
+  return (
+    <nav>
+      <ul>{list}</ul>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      <p>{props.desc}</p>
+    </article>
+  );
+}
+
+// 일반 함수를 이용한 컴포넌트
+function App() {
+  const navArr = [{ title: "html" }, { title: "css" }, { title: "js" }];
+  return (
+    <div className="root">
+      <Header title="리액트(React)" />
+      <Nav nav={navArr} />
+      <Article title="리액트란?" desc="desc1" />
+      <Article title="컴포넌트란?" desc="desc2" />
+      <Article title="props란?" desc="desc3" />
     </div>
   );
 }
 
+// 화살표 함수를 사용한 컴포넌트
+// const App = () => {
+//   return <>hi</>;
+// };
+
+// 내보내기(App을 기본으로 내보내기)
 export default App;
