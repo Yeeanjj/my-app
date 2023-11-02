3ba429f04649e061ca411ac03983c22924076094
Yeeanj <sesju0527@gamil.com>
2023-11-02 13:23:31 +0900

    nav props

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import React from "react";
 
-function Header(props) {
-  console.log(props);
-  return (
-    <header>
-      <h1>logo</h1>
-      {/* <Nav /> */}
-    </header>
-  );
-}
-
-function Nav(props) {
-  // map()함수를 사용한 배열 출력
-  const list = props.nav.map((item, index) => (
-    <li key={index}>
-      <a href={"/sub/" + item.title + "html"}>{item.title}</a>
-    </li>
-  ));
-
-  return (
-    <nav>
-      <ul>{list}</ul>
-    </nav>
-  );
-}
-
-function Article(props) {
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      <p>{props.desc}</p>
-    </article>
-  );
-}
-
 // 일반 함수를 이용한 컴포넌트
 function App() {
   const navArr = [{ title: "html" }, { title: "css" }, { title: "js" }];
   return (
     <div className="root">
-      <Header title="리액트(React)" />
-      <Nav nav={navArr} />
+      <Header title="리액트(React)" nav={navArr} />
       <Article title="리액트란?" desc="desc1" />
       <Article title="컴포넌트란?" desc="desc2" />
       <Article title="props란?" desc="desc3" />
     </div>
+  );
+}
+
+function Header({ title, nav }) {
+  // props.nav = [{ title: "html" }, { title: "css" }, { title: "js" }];
+  // const { title, nav } = props;
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Nav nav={nav} />
+    </header>
+  );
+}
+
+function Nav({ nav }) {
+  // props.nav = [{ title: "html" }, { title: "css" }, { title: "js" }];
+  // const {nav} = props
+  return (
+    <nav>
+      <ul>
+        {nav.map((item, index) => (
+          <li key={index}>
+            <a href={"/sub/" + item.title + "html"}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+  // map()함수를 사용한 배열 출력
+  // const list = props.nav.map((item, index) => (
+  //   <li key={index}>
+  //     <a href={"/sub/" + item.title + "html"}>{item.title}</a>
+  //   </li>
+  // ));
+}
+
+function Article({ title, desc }) {
+  // const { title, desc } = props;
+  return (
+    <article>
+      <h2>{title}</h2>
+      <p>{desc}</p>
+    </article>
   );
 }
 
